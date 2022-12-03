a5cd2c5740b92ae49c5a13d84d34e36786445769
Reemoh <iamreemoh@gmail.com>
2022-12-03 19:45:44 +0100

    modified file with random color feature

--- let_it_snow.py
+++ let_it_snow.py
@@ -14,6 +14,7 @@ def main(speed=0, bg_color="grey"):
   
  
     """TODO: define different colors here"""
+    turtle.colormode(255)
 
 
     for _ in range(10):
@@ -22,6 +23,7 @@ def main(speed=0, bg_color="grey"):
         pos = [np.random.randint(-300, 300), np.random.randint(-300, 300)]
 
         """TODO: set snowflake color here (one of the colors defined above)"""
+        myTurtle.color(np.random.randint(255),np.random.randint(255),np.random.randint(255))
 
         # Go to the start position of the snowflake
         myTurtle.penup()
